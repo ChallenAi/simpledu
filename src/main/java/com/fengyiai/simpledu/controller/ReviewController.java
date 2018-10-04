bef40bad651b7556236fbc19ef5ea1150d3a4cd0
ChallenAi <jianglun465@sina.com>
2018-10-04 10:45:07 +0800

    修改review

--- src/main/java/com/fengyiai/simpledu/controller/ReviewController.java
+++ src/main/java/com/fengyiai/simpledu/controller/ReviewController.java
@@ -1,16 +1,22 @@
 package com.fengyiai.simpledu.controller;
 
+import com.fengyiai.simpledu.requestParams.ReviewParams.AddReviewParams;
+import com.fengyiai.simpledu.requestParams.WikiParams.AddWikiParams;
 import com.fengyiai.simpledu.util.Resp;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class ReviewController {
 
     // 评论或回复评论
     @RequestMapping(value = "/api/review", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
-    public String addReview() {
+    public String addReview(@RequestAttribute Long userId, @Valid @RequestBody AddWikiParams p, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return Resp.RespReqFail("参数错误");
+        }
         return Resp.RespSucc();
     }
 
